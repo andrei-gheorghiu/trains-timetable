41376f0c452109a90211d98f0c6e9d3ad85376cc
Andrei Gheorghiu <andrei.gheorghiu@gamma.ie>
2022-02-04 07:26:54 +0000

    updated justcors prefix

--- src/util/irishRail.api.ts
+++ src/util/irishRail.api.ts
@@ -1,6 +1,6 @@
 export const irishRailApi = {
   prefix(s = ""): string {
-    return `https://justcors.com/tl_e2be2d9/http://api.irishrail.ie/realtime/realtime.asmx/${s}`;
+    return `https://justcors.com/l_1ve8kxkto7/http://api.irishrail.ie/realtime/realtime.asmx/${s}`;
   },
   get baseURL(): string {
     return this.prefix();
